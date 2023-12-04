4d480b7b19a9c6a2306ada7a4c4bc2407ad513f7
pathompat-m <Pathompat.m@gmail.com>
2023-12-04 11:14:22 +0700

    Initial Commit

---
+++ TipCal/TipCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalApp.swift
+//  TipCal
+//
+//  Created by Pathompat Mekbenchapivat on 4/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct TipCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
